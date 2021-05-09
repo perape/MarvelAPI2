96a2c18f04fe679be527257ec47e8e9f8f0a8ac1
Pedro Ramos <otinebordep@gmail.com>
2021-05-08 20:28:51 -0500

    Boton agregar

--- app/src/main/java/edu/itesm/marvelapi/ComicsAdapter.kt
+++ app/src/main/java/edu/itesm/marvelapi/ComicsAdapter.kt
@@ -3,22 +3,37 @@ package edu.itesm.marvelapi
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
-class ComicsAdapter(private val data: List<Comicslist>?) : RecyclerView.Adapter<ComicsAdapter.ViewHolder>()  {
 
-    class ViewHolder(val view: View): RecyclerView.ViewHolder(view){
+class ComicsAdapter(private val data: List<Comicslist>?) : RecyclerView.Adapter<ComicsAdapter.ViewHolder>() {
 
-        fun bind(property: Comicslist){
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bind(property: Comicslist) {
             val title = view.findViewById<TextView>(R.id.tvTitle)
             val imageView = view.findViewById<ImageView>(R.id.imageView)
             val description = view.findViewById<TextView>(R.id.tvDescription)
+            val button = view.findViewById<Button>(R.id.button)
+            val database = Firebase.database
+            val myRef = database.getReference("Base")
+
 
             title.text = property.title
-            description.text = property.description
+            //validar
+            if (property.description == null) {
+                description.text ="Not description"
+            } else {
+                description.text = property.description.substring(0, 100)
+            }
 
             Glide.with(view.context)
                 .load(property.thumbnail.path.plus(".jpg"))
@@ -26,11 +41,34 @@ class ComicsAdapter(private val data: List<Comicslist>?) : RecyclerView.Adapter<
                 .into(imageView)
 
 
+            button.setOnClickListener {
+                if (property.description == null) {
+                    val description2 ="Not description"
+                    agregarcomic(property.title,description2,property.thumbnail,myRef)
+                } else {
+                    agregarcomic(property.title,property.description,property.thumbnail,myRef)
+                }
+            }
+
+        }
+        public fun agregarcomic(title:String,description:String,image:Thumbnail,myRef:DatabaseReference) {
+            // Toast.makeText(itemView.getContext(), "$title", Toast.LENGTH_LONG).show();
+            val compra = Comicslist(
+                title,
+                description,
+                image
+            )
+            val id =myRef.push().key
+            myRef.child(id!!).setValue(compra)
+            Toast.makeText(itemView.getContext(), "se ha agregado al carrito", Toast.LENGTH_LONG).show()
         }
     }
 
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.list_ny_book, parent, false)
+        val v =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_ny_book, parent, false)
         return ViewHolder(v)
     }
 
@@ -40,6 +78,9 @@ class ComicsAdapter(private val data: List<Comicslist>?) : RecyclerView.Adapter<
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(data!![position])
+
+
     }
+
 
 }
